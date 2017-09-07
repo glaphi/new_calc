dc3a3a816a00329e7977e9013f10d3d6fd26b36f
Macbook <macbook@Macbooks-MacBook-Pro.local>
2017-09-07 20:03:00 +0300

    Initial Commit

---
+++ calculator.ios10.forTasksTests/calculator_ios10_forTasksTests.swift
@@ -0,0 +1,36 @@
+//
+//  calculator_ios10_forTasksTests.swift
+//  calculator.ios10.forTasksTests
+//
+//  Created by Glaphi on 07/09/2017.
+//  Copyright © 2017 glaphi. All rights reserved.
+//
+
+import XCTest
+@testable import calculator_ios10_forTasks
+
+class calculator_ios10_forTasksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
